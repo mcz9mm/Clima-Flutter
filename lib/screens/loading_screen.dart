1e14aff9bd94f9e2b202aea965ca43e12c7ee43a
mcz9mm <k.m.swim411@gmail.com>
2020-01-05 18:02:59 +0900

    refactor, update weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,7 @@
 import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
+import 'package:clima/services/weather.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-// TODO: API Key Don't Git Add!
-const apiKey = '';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -24,17 +20,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    latitude = location.latitude;
-    longitude = location.longitude;
 
-    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context){
-      return LocationScreen();
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
     }));
   }
 
